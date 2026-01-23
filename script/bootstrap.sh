3cf5094c9cee881adc7b86b56a3e42d932d8d0d4
Xarag <34477185+Xarag@users.noreply.github.com>
2026-01-23 21:32:44 +0500

    Update bootstrap.sh
    paru

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -3,11 +3,11 @@
 set -xe
 
 if ! command -v yay &>/dev/null; then
-	# sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf ./yay
-	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf ./yay-bin
+	# sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si && cd .. && rm -rf ./paru
+	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/paru.git && cd paru-git && makepkg -si && cd .. && rm -rf ./paru-git
 fi
 
-yay -S --needed stow git
+paru -S --needed stow git
 
 ./script/stow.sh
 ./script/install-packages.sh
